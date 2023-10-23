6ec3dcc40ca9b60e52bc2ad9fcbf9be83d0597b4
woutertack <wouttack1@student.arteveldehs.be>
2023-10-23 17:47:14 +0200

    WIP

--- addons/woutermenno/rating/resources/dist/build/assets/addon-7574c454.js
+++
@@ -1,1 +0,0 @@
-document.addEventListener("DOMContentLoaded",function(n){t()});const t=()=>{const n=document.querySelector("button");n.onclick=function(){alert("Hello world")}};
--- addons/woutermenno/rating/resources/js/addon.js
+++ addons/woutermenno/rating/resources/js/addon.js
@@ -3,10 +3,47 @@ document.addEventListener('DOMContentLoaded', function(event) {
 });
 
 const init = () => {
-const btn = document.querySelector('button');
+  const starContainer = document.getElementById('starContainer');
+  const stars = starContainer.querySelectorAll('.star');
 
-btn.onclick = function() {
-  alert('Hello world');
-};
-};
+  function highlightStars(event) {
+    const mouseX = event.clientX;
 
+    stars.forEach((star, index) => {
+      const starX = star.getBoundingClientRect().left;
+
+      if (mouseX >= starX) {
+        star.classList.add('highlight');
+      } else {
+        star.classList.remove('highlight');
+      }
+    });
+  }
+
+  function resetStars() {
+    stars.forEach((star, index) => {
+      star.classList.remove('highlight');
+      if (index > clickedIndex) {
+        star.classList.remove('active');
+      }
+    });
+  }
+
+  let clickedIndex = -1;
+
+  starContainer.addEventListener('click', (event) => {
+    const clickedStar = event.target;
+    if (clickedStar.classList.contains('star')) {
+      resetStars();
+      clickedIndex = Array.from(stars).indexOf(clickedStar);
+      for (let i = 0; i <= clickedIndex; i++) {
+        stars[i].classList.toggle('active', true);
+      }
+    }
+  });
+
+  stars.forEach((star) => {
+    star.addEventListener('mouseover', highlightStars);
+    star.addEventListener('mouseout', resetStars);
+  });
+};
