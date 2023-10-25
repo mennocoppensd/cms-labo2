5e1de45d50e4cd08225200e40ca728f70ae4dda8
menncopp@student.arteveldehs.be <menncopp@student.arteveldehs.be>
2023-10-25 19:09:34 +0200

    Replaced dropdown with stars hover menu

--- addons/woutermenno/rating/resources/views/cp/index.blade.php
+++ addons/woutermenno/rating/resources/views/cp/index.blade.php
@@ -4,23 +4,41 @@
 
 @section('content')
 <div class="mb-3">
-        <h1 class="">Rating Settings</h1>
-    </div>
+    <h1 class="">Rating Settings</h1>
+</div>
 
-    <form action="{{ cp_route('rating-addon.store') }}" method="POST">
+<form action="{{ cp_route('rating-addon.store') }}" method="POST">
     @csrf
 
     <label for="rating">Select Rating:</label>
-    <select name="rating" id="rating">
-        <option value="1">&#9733;</option>
-        <option value="2">&#9733;&#9733;</option>
-        <option value="3">&#9733;&#9733;&#9733;</option>
-        <option value="4">&#9733;&#9733;&#9733;&#9733;</option>
-        <option value="5">&#9733;&#9733;&#9733;&#9733;&#9733;</option>
-    </select>
+    <div class="rating" id="starContainer">
+        <span class="star" value="1">&#9733;</span>
+        <span class="star" value="2">&#9733;</span>
+        <span class="star" value="3">&#9733;</span>
+        <span class="star" value="4">&#9733;</span>
+        <span class="star" value="5">&#9733;</span>
+    </div>
+
+    <!-- Add an input field for the rating -->
+    <input type="hidden" name="rating" id="rating" value="">
 
     <button type="submit" class="btn-primary">Save</button>
 </form>
 
+<script>
+    // Vanilla JavaScript for star rating
+    const stars = document.querySelectorAll('.star');
+    const ratingInput = document.getElementById('rating');
 
-@endsection
+    stars.forEach(star => {
+        star.addEventListener('click', () => {
+            const value = star.getAttribute('value');
+            ratingInput.value = value;
+            // Highlight the selected stars
+            stars.forEach(s => {
+                s.classList.remove('selected');
+            });
+            star.classList.add('selected');
+        });
+    });
+</script>
